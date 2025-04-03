3a411fc3b8240bfe9dbcc3d293c37f0c850a6499
akshitachordiya <jainakshitachordiya@gmail.com>
2025-04-03 12:35:50 +0530

    added logic to return sum

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,12 @@ class StringCalculator
   
   def add(str)
     return 0 if str.empty?
+
     length_of_string = str.length
     return str.to_i if length_of_string == 1
+
+    str_arr = str.gsub("\n", ",").split(",").reject(&:empty?)
+    num_arr = str_arr.map(&:to_i)
+    return num_arr.sum
   end
 end
