20d511b440b795fe028bcb430ba9c91fe0fce79d
akshitachordiya <jainakshitachordiya@gmail.com>
2025-04-03 11:45:39 +0530

    context for testing if length of string is 1

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe StringCalculator do
         expect(string_calculator.add("")).to eq(0)
       end
     end
+
+    context "when input is a string with length 1" do
+      it "converts the string to integer and return it" do
+        expect(string_calculator.add("5")).to eq(5)
+      end
+    end
   end
 end
