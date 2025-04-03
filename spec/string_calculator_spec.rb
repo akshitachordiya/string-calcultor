93dedb231eb700b3c12ab70db7c1cd9223b1f0b3
akshitachordiya <jainakshitachordiya@gmail.com>
2025-04-03 11:32:36 +0530

    added a describe block in specs for testing add method

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,5 +2,9 @@ require_relative '../string_calculator'
 require 'rspec'
 
 RSpec.describe StringCalculator do
+  let(:string_calculator) { StringCalculator.new }
 
+  describe "#add" do
+
+  end
 end
