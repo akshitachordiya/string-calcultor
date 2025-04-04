6119f12b8ef7c97fc6ae4d25d11582cdf7ab3bd2
akshitachordiya <jainakshitachordiya@gmail.com>
2025-04-04 23:49:46 +0530

    change argument for better readability

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,16 +1,16 @@
 class StringCalculator
   
-  def add(str)
-    return 0 if str.empty?
+  def add(input)
+    return 0 if input.empty?
 
-    return str.to_i if str.length == 1
+    return input.to_i if input.length == 1
     delimiter = /,|\n/
-    if str.start_with?("//")
-      delimiter_line, str = str.split("\n", 2)
+    if input.start_with?("//")
+      delimiter_line, input = input.split("\n", 2)
       custom_delimiter = delimiter_line[2..]
       delimiter = Regexp.escape(custom_delimiter)
     end
-    number_array = str.split(/#{delimiter}/).map(&:to_i)
+    number_array = input.split(/#{delimiter}/).map(&:to_i)
     validate_no_negatives!(number_array)
     return number_array.sum
   end
