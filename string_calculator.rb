c5ca8ecfd26aadc22d459d72443770ec42877a0a
akshitachordiya <jainakshitachordiya@gmail.com>
2025-04-04 21:55:34 +0530

    logic for negative numbers and test custom delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,7 +6,16 @@ class StringCalculator
     length_of_string = str.length
     return str.to_i if length_of_string == 1
     delimiter = /,|\n/
-    str_arr = str.split(/#{delimiter}/).map(&:to_i)
-    return str_arr.sum
+    if str.start_with?("//")
+      delimiter_line, str = str.split("\n", 2)
+      custom_delimiter = delimiter_line[2..]
+      delimiter = Regexp.escape(custom_delimiter)
+    end
+    number_array = str.split(/#{delimiter}/).map(&:to_i)
+    negatives = number_array.select(&:negative?)
+    unless negatives.empty?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
+    return number_array.sum
   end
 end
